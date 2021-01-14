d3218dba0bb49ec255ce467ed5ad395fa6492cee
ReyesCampos <71782217+ReyesCampos@users.noreply.github.com>
2021-01-13 19:46:01 -0700

    Introducción a Prolog

---
+++ logica/holamundo.pl
@@ -0,0 +1,10 @@
+valioso(oro).
+valioso(cobre).
+es_libro(shadowhunters).
+
+progenitor(clara,jose).
+progenitor(tomas,jose).
+progenitor(tomas,isabel).
+progenitor(jose,ana).
+progenitor(jose,patricia).
+progenitor(patricia,jaime).
